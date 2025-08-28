d9a045d545d9f1228229c19bc96a1cc4ebd47a23
Edy Silva <edigleyssonsilva@gmail.com>
2025-08-28 16:15:42 -0300

    Add LSP configuration for TypeScript and Ruby

---
+++ neovim/lua/lsp-definitions.lua
@@ -0,0 +1,9 @@
+local lspconfig = require('lspconfig')
+
+lspconfig.ts_ls.setup{}
+lspconfig.ruby_lsp.setup({
+  init_options = {
+    formatter = 'standard',
+    linters = { 'standard' },
+  },
+})
